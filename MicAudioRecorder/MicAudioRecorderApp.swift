9d0d00dc481d42b118014fae7afe294195f7d7f9
nlemoff@gmail.com <nlemoff@gmail.com>
2025-02-06 14:39:37 -0800

    Initial Commit

---
+++ MicAudioRecorder/MicAudioRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicAudioRecorderApp.swift
+//  MicAudioRecorder
+//
+//  Created by Nicholas Lemoff on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct MicAudioRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MicAudioRecorderTests/MicAudioRecorderTests.swift
@@ -0,0 +1,16 @@
+//
+//  MicAudioRecorderTests.swift
+//  MicAudioRecorderTests
+//
+//  Created by Nicholas Lemoff on 2/6/25.
+//
+
+import Testing
+
+struct MicAudioRecorderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
